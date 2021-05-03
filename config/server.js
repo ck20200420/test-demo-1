93fc6d02ff167d56496250b77ff59d28c86f5ff0
Bryan <ck20200420@gmail.com>
2021-05-03 14:21:44 +0800

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd739acf00daf36ed7fe05e2bca57974d'),
+    },
+  },
+});
